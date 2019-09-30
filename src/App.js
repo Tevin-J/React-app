7901486522949adf00b328f83dfeea10ce55350c
Anton <anton.revta@gmail.com>
2019-09-30 21:09:48 +0300

    browser-router

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import './App.css';
 import Header from './components/Header/Header';
 import NavBar from './components/NavBar/NavBar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import {BrowserRouter, Route} from "react-router-dom";
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+ {/*Загружено дополнение react-router-dom*/}
+
 
 /*Вызываем необходимые компоненты. При помощи компонент все красиво и лаконично, иначе здесь
 было бы много неструктурированной информации, а сейчас она разбита на смысловые блоки и в 
-итоговый файл app.js приходят уже главные компоненты, которые образуют блоки на сайте*/
+итоговый файл app.js приходят уже главные компоненты, которые образуют блоки на сайте*/ 
 const App = () => {
   return (
+    <BrowserRouter>
     <div className='app-wrapper'>
       <Header /> 
       <NavBar />
-      <Profile />
-      </div>
+          <div className='app-wrapper_content'> {/*Добавили класс оформления контента в корневом
+        файле, так как оно будет одинаковое для всех вкладок контента*/}
+            <Route path='/dialogs' component={Dialogs}/> {/*Использовано, чтоб переключаться по вкладкам*/}
+            <Route path='/profile' component={Profile}/>
+            <Route path='/news' component={News}/>
+            <Route path='/music' component={Music}/>
+            <Route path='/settings' component={Settings}/>
+           
+          </div>
+    </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import s from './Profile.module.css';
 import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
-  return <div className={s.content}>
+  return <div>
     <div>
       <img src='https://www.atlasandboots.com/wp-content/uploads/2019/05/feat-image-1-most-beautiful-mountains-in-the-world.jpg' />
     </div>
