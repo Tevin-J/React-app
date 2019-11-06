9ab98b041fb841ceb544edf71dfc6e9ca3c9d7f7
Anton <anton.revta@gmail.com>
2019-11-06 21:17:01 +0300

    Updates

---
+++ precache-manifest.f985155f86600eaaa138340c7bc040c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031d84849c74e2f6a4abc523e5d1a92d",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "20044c83ba9a016ec773",
+    "url": "/react-app/static/css/main.2b02fbe0.chunk.css"
+  },
+  {
+    "revision": "e748fb622917e3550c75",
+    "url": "/react-app/static/js/2.9d8b8286.chunk.js"
+  },
+  {
+    "revision": "20044c83ba9a016ec773",
+    "url": "/react-app/static/js/main.665b124e.chunk.js"
+  },
+  {
+    "revision": "5542c2094e85106f6f95",
+    "url": "/react-app/static/js/runtime-main.ebd43f4f.js"
+  }
+]);
