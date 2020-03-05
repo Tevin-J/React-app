e39995fe59694db734dbe134ac6b96ecf3df64bf
Anton <anton.revta@gmail.com>
2020-03-05 14:10:03 +0300

    change Users component into class. #53

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -24,6 +24,9 @@ const Navbar = (props) => {
                 <NavLink to="/feed" activeClassName={style.activeLink}>News</NavLink>
             </div>
             <div className={style.item}>
+                <NavLink to="/users" activeClassName={style.activeLink}>Users</NavLink>
+            </div>
+            <div className={style.item}>
                 <NavLink to="/music" activeClassName={style.activeLink}>Music</NavLink>
             </div>
             <div className={style.item}>
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,19 +3,21 @@ import style from './Users.module.css'
 import * as axios from "axios";
 import userPhoto from '../../assets/images/user.png'
 
-let Users = (props) => {
-
-    if (props.users.length === 0) { /*если изначально длина массива юзеров равна нулю, то делаем get-запрос на сервер,
-    и затем вызываем коллбек который засетает юзеров, полученных с сервера*/
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
-        })
+class Users extends React.Component {
+    constructor(props) {
+        super(props)
+        if (this.props.users.length === 0) { /*если изначально длина массива юзеров равна нулю, то делаем get-запрос на сервер,
+             и затем вызываем коллбек который засетает юзеров, полученных с сервера*/
+            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+                this.props.setUsers(response.data.items)
+            })
+        }
     }
-
-    return (
-        <div>
-            {
-                props.users.map(user => <div key={user.id}>
+    render() {
+        return (
+            <div>
+                {
+                    this.props.users.map(user => <div key={user.id}>
                     <span>
                         <div className={style.userPhoto}>
                             <img src={user.photos.small ? user.photos.small : userPhoto} className={style.userPhoto}/>
@@ -23,29 +25,31 @@ let Users = (props) => {
                         <div>
                             {user.followed
                                 ? <button onClick={() => {
-                                    props.unfollow(user.id)
+                                    this.props.unfollow(user.id)
                                 }}>Unfollow</button>
                                 : <button onClick={() => {
-                                    props.follow(user.id)
+                                    this.props.follow(user.id)
                                 }}>Follow</button>}
 
                         </div>
                     </span>
-                        <span>
+                            <span>
                         <span>
                             <div>{user.name}</div>
                             <div>{user.status}</div>
                         </span>
-                        {/*<span>
+                                {/*<span>
                             <div>{user.location.country}</div>
                             <div>{user.location.city}</div>
                         </span>*/}
                     </span>
-                    </div>
-                )
-            }
-        </div>
-    )
+                        </div>
+                    )
+                }
+            </div>
+        )
+    }
+
 }
 
 export default Users
