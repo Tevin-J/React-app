42a83093b7f6aead0f89ad154a6e8c6961eec59c
Anton <anton.revta@gmail.com>
2020-03-17 23:50:14 +0300

    redux-form. #75

--- src/Redux/reduxStore.js
+++ src/Redux/reduxStore.js
@@ -4,14 +4,16 @@ import dialogsReducer from "./dialogsReducer";
 import sidebarReducer from "./sidebarReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
-import thunkMiddleware from 'redux-thunk'
+import thunkMiddleware from 'redux-thunk';
+import {reducer as formReducer} from 'redux-form'
 
 let reducers = combineReducers({ //combineReducers объединяет reducerы вместе с их ветками стейта
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     sidebar: sidebarReducer,
     usersPage: usersReducer,
-    auth: authReducer
+    auth: authReducer,
+    form: formReducer //обязательно ключ должен быть назван form, потому что библиотека redux-form будет в стейте искать именно form
 });
 let store = createStore(reducers, applyMiddleware(thunkMiddleware)); /*redux создает store с помощью команды
 createStore, добавляем прослойку thunkMiddleware для того, чтоб BLL мог выполнять запросы на сервер*/
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import {Field, reduxForm} from "redux-form";
 
-const Login = (props) => {
+const LoginForm = (props) => {
     return (
-        <h1>Login</h1>
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <Field placeholder={'Login'} name={'login'} component={'input'}/> {/*вместо инпутов пишем тег из
+                    redux-form Field, в котором указываем в атрибуте component то, что хотим отрисовать. атрибут name
+                    обязательный*/}
+                </div>
+                <div>
+                    <Field placeholder={'Password'} name={'password'} component={'input'}/>
+                </div>
+                <div>
+                    <Field component={'input'} name={'rememberMe'} type={'checkbox'}/> remember me
+                </div>
+                <div>
+                    <button>Sign in</button>
+                </div>
+            </form>
+    )
+}
+const LoginReduxForm = reduxForm({ /*связываем LoginForm c redux-form*/
+    form: 'login' /*уникальное имя для формы, которую мы оборачиваем HOCom reduxForm*/
+})(LoginForm)
+const Login = (props) => {
+    const onSubmit = (formData) => { /*в эту ф-ю приходят все данные из формы*/
+        console.log(formData)
+    }
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit}/> {/*необходимо для сабмита прокинуть в HOC onSubmit. в него придут
+            данные из формы, которую мы заполнили*/}
+        </div>
+
     )
 }
 export default Login
