b7c751ed9b23172b9bb1f0b61ab7d379bc419d1f
Anton <anton.revta@gmail.com>
2020-04-08 23:28:06 +0300

    react.lazy. #94

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, Suspense} from 'react';
 import './App.css';
 import Navbar from "./components/NavBar/Navbar";
 import {HashRouter, Route, withRouter} from "react-router-dom";
@@ -6,9 +6,7 @@ import Feed from "./components/Content/Feed/Feed";
 import Music from "./components/Content/Music/Music";
 import Settings from "./components/Content/Settings/Settings";
 import PropTypes from 'prop-types';
-import DialogsContainer from "./components/Content/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Content/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import {compose} from "redux";
@@ -16,7 +14,10 @@ import {connect, Provider} from "react-redux";
 import {initializeApp} from "./Redux/appReducer";
 import Preloader from "./components/Common/Preloader/Preloader";
 import store from "./Redux/reduxStore";
-
+import {withSuspense} from "./hoc/withSuspense";
+/*с помощью React.lazy загружаем некоторые компоненты не сразу, а только когда они будут необходимы*/
+const ProfileContainer = React.lazy(() => import('./components/Content/Profile/ProfileContainer'));
+const DialogsContainer = React.lazy(() => import('./components/Content/Dialogs/DialogsContainer'));
 
 class App extends Component {
     componentDidMount() {
@@ -32,9 +33,9 @@ class App extends Component {
                 <Navbar/>
                 <div className="app-wrapper-content"> {/*создали данный класс, так как он общий для всего контента, и
                 вынесли его на более высокий уровень, чтоб не прописывать в css-модуле каждой компоненты контента отдельно*/}
-                    <Route path='/dialogs' render={() => <DialogsContainer/>}/> {/*работа тэга route: когда встечает заданный путь,
+                    <Route path='/dialogs' render={withSuspense(DialogsContainer)}/> {/*работа тэга route: когда встечает заданный путь,
                     отрисовывает заданную компоненту. вместо метода component используем render, чтоб передать в компоненты пропсы*/}
-                    <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/> {/*с помощью :userId
+                    <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/> {/*с помощью :userId
                     показываем, что в урле profile должен быть параметр userId, а с помощью ? - что этот параметр -
                     опциональный и его может не быть(например для отрисовки своей страницы профиля)*/}
                     <Route path='/users' render={() => <UsersContainer/>}/>
